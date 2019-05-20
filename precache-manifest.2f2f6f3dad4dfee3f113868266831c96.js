c4fca935f0fe2363925e1a464a38e90a5e3af527
crcass <ccass@live.com>
2019-05-19 23:06:08 -0500

    Updates

---
+++ precache-manifest.2f2f6f3dad4dfee3f113868266831c96.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e9793324470e1ba6570f649fef0b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4ed94cb3b00043632b3",
+    "url": "/static/css/main.6d79c1df.chunk.css"
+  },
+  {
+    "revision": "55b79d3eaca1651395de",
+    "url": "/static/js/2.12759145.chunk.js"
+  },
+  {
+    "revision": "a4ed94cb3b00043632b3",
+    "url": "/static/js/main.d9631163.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1db7e743a4b3cefbbafdd5636e47f4d2",
+    "url": "/static/media/bamazon.1db7e743.png"
+  },
+  {
+    "revision": "05610ab5f7d01ae20be3c82cc88578f4",
+    "url": "/static/media/edb.05610ab5.png"
+  },
+  {
+    "revision": "4da8bd0a9c240d8ca6b8f444a2367f1e",
+    "url": "/static/media/eplMemory.4da8bd0a.png"
+  },
+  {
+    "revision": "5987d7af50a6f6be8738df7a1acf3a55",
+    "url": "/static/media/gbSearch.5987d7af.png"
+  },
+  {
+    "revision": "47055656755428e907a185e3b77ba7d1",
+    "url": "/static/media/hail.47055656.png"
+  },
+  {
+    "revision": "8d5523b277fda9f275c4c5eb5c948dd0",
+    "url": "/static/media/hailCLI.8d5523b2.png"
+  },
+  {
+    "revision": "3c6b462390ae55aabf93469a0c5446ef",
+    "url": "/static/media/link.3c6b4623.png"
+  },
+  {
+    "revision": "a17773ab597665491641b8ad6f3be7bc",
+    "url": "/static/media/liri.a17773ab.png"
+  },
+  {
+    "revision": "3dba973c09c72cf00493aa9c38ecd48d",
+    "url": "/static/media/profile.3dba973c.jpg"
+  },
+  {
+    "revision": "7057c27cb8969b92e7aa16b919778f0a",
+    "url": "/static/media/rps.7057c27c.png"
+  },
+  {
+    "revision": "1f078f500bc15ee14d3259f2598b7657",
+    "url": "/static/media/signal.1f078f50.png"
+  },
+  {
+    "revision": "a457abdcaab89a09090731c42a7e88bc",
+    "url": "/static/media/starfighter.a457abdc.png"
+  },
+  {
+    "revision": "0b4adcc09b7c43e3672932c36b36cea0",
+    "url": "/static/media/supersearch.0b4adcc0.png"
+  },
+  {
+    "revision": "a2f3bc87208117c6ddb25faec36a2aaf",
+    "url": "/static/media/trivia.a2f3bc87.png"
+  },
+  {
+    "revision": "469124bf421d69b69f709899942fb3f9",
+    "url": "/static/media/vNews.469124bf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e4ce1449c26e326dcd2109b54c99d15e.js"
+  "/precache-manifest.2f2f6f3dad4dfee3f113868266831c96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
