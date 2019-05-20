d1524951c749a00a1d4a9545c64c476a15703ad6
crcass <ccass@live.com>
2019-05-19 23:02:50 -0500

    Updates

---
+++ precache-manifest.e4ce1449c26e326dcd2109b54c99d15e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ec76e4af67c3133a86ea3352d68eff",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b621f4affd535d26921a",
+    "url": "/portfolio/static/css/main.6d79c1df.chunk.css"
+  },
+  {
+    "revision": "3e3682cd245b8c26c186",
+    "url": "/portfolio/static/js/2.89a61246.chunk.js"
+  },
+  {
+    "revision": "b621f4affd535d26921a",
+    "url": "/portfolio/static/js/main.5ec94dcb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "1db7e743a4b3cefbbafdd5636e47f4d2",
+    "url": "/portfolio/static/media/bamazon.1db7e743.png"
+  },
+  {
+    "revision": "05610ab5f7d01ae20be3c82cc88578f4",
+    "url": "/portfolio/static/media/edb.05610ab5.png"
+  },
+  {
+    "revision": "4da8bd0a9c240d8ca6b8f444a2367f1e",
+    "url": "/portfolio/static/media/eplMemory.4da8bd0a.png"
+  },
+  {
+    "revision": "5987d7af50a6f6be8738df7a1acf3a55",
+    "url": "/portfolio/static/media/gbSearch.5987d7af.png"
+  },
+  {
+    "revision": "47055656755428e907a185e3b77ba7d1",
+    "url": "/portfolio/static/media/hail.47055656.png"
+  },
+  {
+    "revision": "8d5523b277fda9f275c4c5eb5c948dd0",
+    "url": "/portfolio/static/media/hailCLI.8d5523b2.png"
+  },
+  {
+    "revision": "3c6b462390ae55aabf93469a0c5446ef",
+    "url": "/portfolio/static/media/link.3c6b4623.png"
+  },
+  {
+    "revision": "a17773ab597665491641b8ad6f3be7bc",
+    "url": "/portfolio/static/media/liri.a17773ab.png"
+  },
+  {
+    "revision": "3dba973c09c72cf00493aa9c38ecd48d",
+    "url": "/portfolio/static/media/profile.3dba973c.jpg"
+  },
+  {
+    "revision": "7057c27cb8969b92e7aa16b919778f0a",
+    "url": "/portfolio/static/media/rps.7057c27c.png"
+  },
+  {
+    "revision": "1f078f500bc15ee14d3259f2598b7657",
+    "url": "/portfolio/static/media/signal.1f078f50.png"
+  },
+  {
+    "revision": "a457abdcaab89a09090731c42a7e88bc",
+    "url": "/portfolio/static/media/starfighter.a457abdc.png"
+  },
+  {
+    "revision": "0b4adcc09b7c43e3672932c36b36cea0",
+    "url": "/portfolio/static/media/supersearch.0b4adcc0.png"
+  },
+  {
+    "revision": "a2f3bc87208117c6ddb25faec36a2aaf",
+    "url": "/portfolio/static/media/trivia.a2f3bc87.png"
+  },
+  {
+    "revision": "469124bf421d69b69f709899942fb3f9",
+    "url": "/portfolio/static/media/vNews.469124bf.png"
+  }
+]);
